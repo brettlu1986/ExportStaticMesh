c7bfa3fd8b93a39f6fd0970cd45a8068572040b6
brettlu1986 <LUZHENG2@kingsoft.com>
2021-06-23 10:48:39 +0800

    change project name

--- copy.bat
+++ copy.bat
@@ -1,12 +1,12 @@
-copy .\ExportStaticMesh\Saved\Export\Animations\*.bin .\LoadStaticMesh\LoadStaticMesh\Resource\Animations\
-copy .\ExportStaticMesh\Saved\Export\Cameras\*.bin .\LoadStaticMesh\LoadStaticMesh\Resource\Cameras\
-copy .\ExportStaticMesh\Saved\Export\Lights\*.bin .\LoadStaticMesh\LoadStaticMesh\Resource\Lights\
-copy .\ExportStaticMesh\Saved\Export\Maps\*.bin .\LoadStaticMesh\LoadStaticMesh\Resource\Maps\
-copy .\ExportStaticMesh\Saved\Export\Materials\*.bin .\LoadStaticMesh\LoadStaticMesh\Resource\Materials\
-copy .\ExportStaticMesh\Saved\Export\SkeletalMeshes\*.bin .\LoadStaticMesh\LoadStaticMesh\Resource\SkeletalMeshes\
-copy .\ExportStaticMesh\Saved\Export\StaticMeshes\*.bin .\LoadStaticMesh\LoadStaticMesh\Resource\StaticMeshes\
-copy .\ExportStaticMesh\Saved\Export\Textures\*.dds .\LoadStaticMesh\LoadStaticMesh\Resource\Textures\
-copy .\ExportStaticMesh\Saved\Export\AssetInfo\*.bin .\LoadStaticMesh\LoadStaticMesh\Resource\AssetInfo\
+copy .\ExportStaticMesh\Saved\Export\Animations\*.bin .\EngineSamples\LoadSampleMap\Resource\Animations\
+copy .\ExportStaticMesh\Saved\Export\Cameras\*.bin .\EngineSamples\LoadSampleMap\Resource\Cameras\
+copy .\ExportStaticMesh\Saved\Export\Lights\*.bin .\EngineSamples\LoadSampleMap\Resource\Lights\
+copy .\ExportStaticMesh\Saved\Export\Maps\*.bin .\EngineSamples\LoadSampleMap\Resource\Maps\
+copy .\ExportStaticMesh\Saved\Export\Materials\*.bin .\EngineSamples\LoadSampleMap\Resource\Materials\
+copy .\ExportStaticMesh\Saved\Export\SkeletalMeshes\*.bin .\EngineSamples\LoadSampleMap\Resource\SkeletalMeshes\
+copy .\ExportStaticMesh\Saved\Export\StaticMeshes\*.bin .\EngineSamples\LoadSampleMap\Resource\StaticMeshes\
+copy .\ExportStaticMesh\Saved\Export\Textures\*.dds .\EngineSamples\LoadSampleMap\Resource\Textures\
+copy .\ExportStaticMesh\Saved\Export\AssetInfo\*.bin .\EngineSamples\LoadSampleMap\Resource\AssetInfo\
 
 
 pause
